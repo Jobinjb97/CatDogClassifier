4c07e2a3b1d94dfaea7befa20264cbe839a2a87a
unknown <jobinjb97@gmail.com>
2023-06-17 13:25:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import load_model
+from PIL import Image
+
+
+st.title('Cat Dog Classifier')
+
+#step1: load the model
+
+model=load_model('cats_dogs_small_2.h5')
+
+#load the image to predict
+
+file_uploaded=st.file_uploader('Choose the Database',accept_multiple_files=False)
+
+if file_uploaded is not None:
+    file_name=file_uploaded
+else:
+    file_name='cat.1.jpg'
+
+if st.checkbox('View Image',False):
+    image=Image.open(file_name)
+    st.image(image)
+
+    
+#step3 :preprocess the image
+
+img=load_img(file_name,target_size=(150,150))
+img_array=img_to_array(img)
+img_array_final=np.expand_dims(img_array,axis=0)
+
+
+#step 4: predict the image and print the result
+
+prediction=int(model.predict(img_array_final)[0][0])
+if st.button('PREDICT'):
+    if prediction==1:
+        st.subheader('The image  is a Dog')
+    else:
+        st.subheader('The image is a Cat')
